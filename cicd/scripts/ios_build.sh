e30bcc29cf14bf0f69dfef6b6da583abd7b1981d
CavanSu <403029552@qq.com>
2020-03-31 14:11:34 +0800

    ios mac ci

---
+++ cicd/scripts/ios_build.sh
@@ -0,0 +1,36 @@
+WORKING_PATH=$1
+APP_TARGET=$2
+
+echo "WORKING_PATH: ${WORKING_PATH}"
+echo "APP_TARGET: ${APP_TARGET}"
+
+cd ${WORKING_PATH}
+echo `pwd`
+
+rm -f *.ipa
+rm -rf *.app
+rm -f *.zip
+rm -rf dSYMs
+rm -rf *.dSYM
+rm -f *dSYMs.zip
+rm -rf *.xcarchive
+
+BUILD_DATE=`date +%Y-%m-%d-%H.%M.%S`
+ArchivePath=${APP_TARGET}-${BUILD_DATE}.xcarchive
+
+TARGET_FILE=""
+if [ ! -f "Podfile" ];then
+TARGET_FILE="${APP_TARGET}.xcodeproj"
+xcodebuild clean -project ${TARGET_FILE} -scheme "${APP_TARGET}" -configuration Release
+xcodebuild -project ${TARGET_FILE} -scheme "${APP_TARGET}" -archivePath ${ArchivePath} archive
+xcodebuild -exportArchive -exportOptionsPlist exportPlist.plist -archivePath ${ArchivePath} -exportPath .
+else
+pod install
+TARGET_FILE="${APP_TARGET}.xcworkspace"
+xcodebuild clean -workspace ${TARGET_FILE} -scheme "${APP_TARGET}" -configuration Release
+xcodebuild -workspace ${TARGET_FILE} -scheme "${APP_TARGET}" -archivePath ${ArchivePath} archive
+xcodebuild -exportArchive -exportOptionsPlist exportPlist.plist -archivePath ${ArchivePath} -exportPath .
+fi
+
+
+
